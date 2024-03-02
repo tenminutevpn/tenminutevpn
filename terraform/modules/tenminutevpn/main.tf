6e90f459d817c2badb387607deec098bebdbfa02
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-02 17:46:24 +0400

    added tls_private_key, digitalocean_ssh_key, and digitalocean_droplet resources to main.tf, added ipv4_address output to outputs.tf, included tls provider in providers.tf, and updated .terraform.lock.hcl with hashicorp/tls details

--- terraform/modules/tenminutevpn/main.tf
+++ terraform/modules/tenminutevpn/main.tf
@@ -4,3 +4,22 @@ resource "digitalocean_custom_image" "this" {
   distribution = "Debian"
   regions      = [data.digitalocean_region.this.slug]
 }
+
+resource "tls_private_key" "this" {
+  algorithm = "ED25519"
+}
+
+resource "digitalocean_ssh_key" "this" {
+  name       = "tenminutevpn"
+  public_key = tls_private_key.this.public_key_openssh
+}
+
+resource "digitalocean_droplet" "this" {
+  name   = "tenminutevpn"
+  image  = digitalocean_custom_image.this.id
+  region = data.digitalocean_region.this.slug
+  size   = "s-1vcpu-512mb-10gb"
+  ssh_keys = [
+    digitalocean_ssh_key.this.fingerprint,
+  ]
+}
--- terraform/modules/tenminutevpn/providers.tf
+++ terraform/modules/tenminutevpn/providers.tf
@@ -10,5 +10,9 @@ terraform {
       source  = "integrations/github"
       version = "~> 5.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
