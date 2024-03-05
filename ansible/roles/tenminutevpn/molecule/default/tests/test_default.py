5808385c5c40f8151284c8c546c87312e92e684a
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-05 12:51:05 +0400

    added wireguard installation check in tenminutevpn tests

--- ansible/roles/tenminutevpn/molecule/default/tests/test_default.py
+++ ansible/roles/tenminutevpn/molecule/default/tests/test_default.py
@@ -5,3 +5,8 @@ def test_users(host):
     """Validate users."""
     assert host.user("tenminutevpn").exists
     assert host.user("tenminutevpn").group == "tenminutevpn"
+
+
+def test_wireguard(host):
+    """Check if WireGuard is installed."""
+    assert host.package("wireguard").is_installed
