8e4ec6118b4175a11ffa9421632bf3b83f034a66
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-05 13:42:05 +0400

    added tests to check wireguard configuration file existence and validity

--- ansible/roles/tenminutevpn/molecule/default/tests/test_default.py
+++ ansible/roles/tenminutevpn/molecule/default/tests/test_default.py
@@ -10,3 +10,10 @@ def test_users(host):
 def test_wireguard(host):
     """Check if WireGuard is installed."""
     assert host.package("wireguard").is_installed
+
+def test_wireguard_configuration(host):
+    assert host.file("/etc/wireguard/wg0.conf").exists
+    assert host.file("/etc/wireguard/wg0.conf").is_file
+
+    assert host.file("/root/.wireguard/client.conf").exists
+    assert host.file("/root/.wireguard/client.conf").is_file
