6a8be28ae09b07c3f265b6e08b4fa9283b1bd6d4
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-05 12:38:19 +0400

    updated wireguard ansible role tests, added cache_valid_time, removed systemd restart

--- ansible/roles/wireguard/molecule/default/tests/test_default.py
+++ ansible/roles/wireguard/molecule/default/tests/test_default.py
@@ -1,10 +1,14 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_wireguard_package(host):
+    """Check if WireGuard is installed."""
+    assert host.package("wireguard").is_installed
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+
+def test_wireguard_scripts(host):
+    """Check if WireGuard scripts are installed."""
+    for script in ["wireguard-keygen.sh", "wireguard-init.sh"]:
+        assert host.file(f"/usr/local/bin/{script}").exists
+        assert host.file(f"/usr/local/bin/{script}").is_file
+        assert host.file(f"/usr/local/bin/{script}").mode == 0o744
