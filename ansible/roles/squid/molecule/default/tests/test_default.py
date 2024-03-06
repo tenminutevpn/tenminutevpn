4d5fdd3c8ee57fbf7b1b9c1c8d31309b29fcf776
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-06 12:42:42 +0400

    updated squid role tests and configuration file name and path

--- ansible/roles/squid/molecule/default/tests/test_default.py
+++ ansible/roles/squid/molecule/default/tests/test_default.py
@@ -1,10 +1,11 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_squid_script_exists(host):
+    with host.sudo():
+        assert host.file("/usr/local/bin/squid-init.sh").exists
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+
+def test_squid_cloud_init_script_exists(host):
+    with host.sudo():
+        assert host.file("/etc/cloud/cloud.cfg.d/98_squid.cfg").exists
