4d4001b6275086fb2ed86f3c9de6c602f08aa6b5
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-12 22:44:18 +0400

    refactored wireguard tests and added squid tests in acceptance testing role

--- ansible/roles/acceptance_testing/molecule/default/tests/test_default.py
+++ ansible/roles/acceptance_testing/molecule/default/tests/test_default.py
@@ -9,18 +9,18 @@ def test_users(host):
 
 def test_wireguard(host):
     """Check if WireGuard is installed."""
-    assert host.package("wireguard").is_installed
-
-
-def test_wireguard_configuration(host):
     with host.sudo():
+        assert host.package("wireguard").is_installed
         assert host.file("/etc/wireguard/wg0.conf").exists
         assert host.file("/etc/wireguard/wg0.conf").is_file
-
-        assert host.file("/root/.wireguard/client.conf").exists
-        assert host.file("/root/.wireguard/client.conf").is_file
-
-
-def test_wireguard_port(host):
-    with host.sudo():
         assert host.socket("udp://51820").is_listening
+
+
+def test_squid(host):
+    with host.sudo():
+        assert host.package("squid").is_installed
+        assert host.file("/etc/squid/squid.conf").exists
+        assert host.file("/etc/squid/squid.conf").is_file
+        assert host.service("squid").is_enabled
+        assert host.service("squid").is_running
+        assert host.socket("tcp://3128").is_listening
