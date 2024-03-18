8d0d99d2c36d1f866d044699f1401de41fd932e6
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-19 00:09:08 +0400

    updated test_default.py, replaced hosts file test with tenminutevpn_manager installation check

--- ansible/roles/tenminutevpn_manager/molecule/default/tests/test_default.py
+++ ansible/roles/tenminutevpn_manager/molecule/default/tests/test_default.py
@@ -1,10 +1,11 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_tenminutevpn_manager(host):
+    """ Validate tenminutevpn_manager is installed """
+    f = host.file("/usr/bin/tenminutevpn-manager")
 
     assert f.exists
     assert f.user == "root"
     assert f.group == "root"
+    assert f.mode == 0o755
